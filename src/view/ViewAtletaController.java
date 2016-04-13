c328a2fa4f908c57d08a3ed44930a663c5e241c2
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-04-13 00:59:28 -0300

    Implementação

---
+++ src/view/ViewAtletaController.java
@@ -0,0 +1,39 @@
+package view;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewAtletaController {
+
+	 	@FXML
+	    private Button btnPesquisarAtleta;
+
+	    @FXML
+	    private Button btnIncluirAtleta;
+
+	    @FXML
+	    private Button btnAlterarAtleta;
+
+	    @FXML
+	    private Button btnExcluirAtleta;
+
+	    @FXML
+	    void alterarAtleta(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void excluirAtleta(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void incluirAtleta(ActionEvent event) {
+
+	    }
+
+	    @FXML
+	    void pesquisarAtleta(ActionEvent event) {
+
+	    }
+}
---
+++ src/view/ViewPrincipalControler.java
@@ -0,0 +1,24 @@
+package view;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ViewPrincipalControler {
+	
+	@FXML
+    private Button btnAtletas;
+
+    @FXML
+    private Button btnCampos;
+
+    @FXML
+    void chamarAtletas(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void chamarCampos(ActionEvent event) {
+    	
+    }
+}
+
