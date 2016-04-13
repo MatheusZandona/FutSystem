ea378ea368a1c8742a2c4a3eb407388105cf63bd
Matheus Zandoná <zan_matheus@yahoo.com.br>
2016-04-13 00:09:40 -0300

    Criado a classe Campo.

---
+++ src/Classes/Campo.java
@@ -0,0 +1,42 @@
+package Classes;
+
+public class Campo {
+
+	private int codigo;
+	private String email;
+	private String senha;
+	private String tipoCampo;
+
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getTipoCampo() {
+		return tipoCampo;
+	}
+
+	public void setTipoCampo(String tipoCampo) {
+		this.tipoCampo = tipoCampo;
+	}
+	
+}
